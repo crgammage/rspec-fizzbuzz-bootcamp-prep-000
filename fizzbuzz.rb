782c9e6bbe16e87f74706dca37f1ed516d0f36ff
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 14:49:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,4 @@
-
-  it "returns 'Fizz' when the number is divisible by 3" do
+it "returns 'Fizz' when the number is divisible by 3" do
     if int % 3 === 0
       puts "Fizz"
   elseif int % 5 === 0
