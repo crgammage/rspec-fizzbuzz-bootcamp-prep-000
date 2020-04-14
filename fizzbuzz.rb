013c2f8138e1e792106aaceecf53f65c4c9112e0
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 15:17:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(num)
     if (num % 3 == 0) && (num % 5 == 0)
       puts "FizzBuzz"
-    elseif (num % 5 == 0)
+    if (num % 5 == 0)
       puts "Buzz"
-    else (num % 3 == 0)
+    if (num % 3 == 0)
       puts "Fizz"
     end
