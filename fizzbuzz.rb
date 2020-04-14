eb034ff1f4b16017bbc16f3c89c3fab5a5b02d94
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 14:53:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,1 @@
-def fizzbuzz(max):
-  1.upto(max).each do |n|
-    line = ''
-    line << 'Fizz' if n % 3 == 0
-    line << 'Buzz' if n % 5 == 0
-    line = n if line.empty?
-    p line
-  end
-end
+def fizzbuzz(int)
