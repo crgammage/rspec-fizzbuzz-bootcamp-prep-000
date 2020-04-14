a4e1d4ca02ff435f1997ac55e52b711b5a230f33
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 14:54:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,17 @@
-def fizzbuzz(int)
-  array = []
-  if int % 3 === 0
-    int << array
+def fizzbuzz(num)
+  1.upto(num).each do |x| 
+    if x%3 == 0 && x%5 == 0 
+      p "fizzbuzz"
+    elsif x % 5 == 0 
+      p "buzz"
+    elsif x % 3 == 0 
+      p "fizz"
+    else
+      p x
+    end
+  end
+end
+
+print "Type Fuzzbuzz range? (1-_): "
+max = gets.chomp
+fizzbuzz(max.to_i)
