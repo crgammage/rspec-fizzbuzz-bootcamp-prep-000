c22666c8010b79eb0f85878d357c8526102e36c1
masterful-array-3850 <crgammage@gmail.com>
2020-04-14 14:57:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz(num)
   1.upto(num).each do |x|
     if (x % 3 == 0) && (x % 5 == 0)
+      puts "FizzBuzz"
+    elseif (x % 5 ===0)
+      puts "Buzz"
+    else (x % 3 === 0)
       puts "Fizz"
-    elseif (x % 5 ===0) 
